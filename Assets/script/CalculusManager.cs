6519be0298b606b362f60b9d88bb977e2feb038b
Greent_WU <lynn84961@gmail.com>
2017-12-14 20:43:25 +0800

    Script of calculator manager added

---
+++ Assets/script/CalculusManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculusManager : MonoBehaviour {
+
+	public GameObject C;
+
+	// Use this for initialization
+	void Start () {
+		C.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void CalculatorOn(){
+		C.SetActive (true);
+	}
+	public void CalculatorOff(){
+		C.SetActive (false);
+	}
+
+}
---
+++ Assets/script/compareAns.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class countAns : MonoBehaviour {
+
+	public GameObject SkillFrameStairs;
+	public GameObject SkillFramePCH;
+	public GameObject SkillStairs;
+	public GameObject SkillP;
+	public GameObject SkillC;
+	public GameObject SkillH;
+	public GameObject NumberFrameStairs;
+	public GameObject NumberFramePCH1;
+	public GameObject NumberFramePCH2;
+
+
+	public GameObject SkillChartStairs;
+	public GameObject SkillChartP;
+	public GameObject SkillChartC;
+	public GameObject SkillChartH;
+	public GameObject NW;
+	public GameObject ansBUt;
+
+	[SerializeField] private Text NumStairsT = null;
+	[SerializeField] private Text NumPCH01T = null;
+	[SerializeField] private Text NumPCH02T = null;
+	[SerializeField] private Text Result = null;
+
+	public class answerData
+	{
+		public string func;
+		public int n;
+		public int m;
+		public int mWrong;
+	}
+
+	public void load()
+	{
+		//讀取json檔案並轉存成文字格式
+		StreamReader file = new StreamReader(System.IO.Path.Combine(Application.streamingAssetsPath, "Answers"));
+		string loadJson = file.ReadToEnd();
+		file.Close();
+
+		//新增一個物件類型為playerState的變數 loadData
+		answerData loadData = new answerData();
+
+		//使用JsonUtillty的FromJson方法將存文字轉成Json
+		loadData = JsonUtility.FromJson<answerData>(loadJson);
+
+		//驗證用，將sammaru的位置變更為json內紀錄的位置
+		//GameObject.Find("sammaru").transform.position = loadData.pos;
+	}
+
+	public void countResult(){
+
+
+		int result = 0;
+
+		Debug.Log (GameObject.Find ("Selected").GetComponent<calculator> ().FrameOn);
+		if (GameObject.Find ("Selected").GetComponent<calculator> ().FrameOn == 1) {
+			int n = int.Parse (NumStairsT.text);	
+			result = countStairs (n);
+			PrintStairs (result, n);
+		} else if (GameObject.Find ("Selected").GetComponent<calculator> ().FrameOn == 2) {
+			int n = int.Parse (NumPCH01T.text);
+			int m = int.Parse (NumPCH02T.text);
+			result = countP (n, m);
+			PrintP (result,n , m);
+		} else if (GameObject.Find ("Selected").GetComponent<calculator> ().FrameOn == 3) {
+			int n = int.Parse (NumPCH01T.text);
+			int m = int.Parse (NumPCH02T.text);
+			result = countC (n, m);
+			PrintC (result, n, m);
+		}
+		else if (GameObject.Find ("Selected").GetComponent<calculator> ().FrameOn == 4) {
+			int n = int.Parse (NumPCH01T.text);
+			int m = int.Parse (NumPCH02T.text);
+			result = countH (n, m);
+			PrintH (result, n, m, n+m-1);
+		}
+
+
+		SkillFrameStairs.SetActive (false);
+		SkillFramePCH.SetActive (false);
+		SkillStairs.SetActive (false);
+		SkillP.SetActive (false);
+		SkillC.SetActive (false);
+		SkillH.SetActive (false);
+		NumberFrameStairs.SetActive (false);
+		NumberFramePCH1.SetActive (false);
+		NumberFramePCH2.SetActive (false);
+		NW.SetActive (false);
+
+		SkillChartStairs.SetActive (true);
+		SkillChartP.SetActive (true);
+		SkillChartC.SetActive (true);
+		SkillChartH.SetActive (true);
+
+		SkillChartStairs.GetComponent<Button> ().enabled = false;
+		SkillChartP.GetComponent<Button> ().enabled = false;
+		SkillChartC.GetComponent<Button> ().enabled = false;
+		SkillChartH.GetComponent<Button> ().enabled = false;
+
+		ansBUt.GetComponent<Image> ().enabled = false;
+		ansBUt.GetComponent<Button> ().enabled = false;
+
+	}
+
+	int countStairs(int n){
+		if (n == 0) {
+			return 1;
+		} else {
+			int ans = n;
+			for (int i = 1; i < n; i++) {
+				ans *= i;
+			}
+			return ans;
+		}
+	}
+	int countP(int n, int m){
+		int ans = 0;
+		ans = countStairs (n) / countStairs (n - m);
+		return ans;
+	}
+	int countC(int n, int m){
+		int ans = 0;
+		ans = countStairs (n) / (countStairs (n - m) * countStairs(m));
+		return ans;
+	}
+	int countH(int n, int m){
+		int ans = 0;
+		ans = countC (n + m - 1, m);
+		return ans;
+	}
+
+	void PrintStairs(int result, int n){
+		string p = "";
+
+		p= n+"!\n= ";
+
+		if (n == 0) {
+			p += "1";
+		} else {
+
+			for (int i = n; i > 1; i--) {
+				p += i + " * ";
+			}
+			p += "1\n= " + result;
+		}
+		Result.text = p;
+	}
+
+	void PrintP(int result, int n, int m){
+		string p = "";
+		p = "P(" + n + ", " + m + ")\n= " + n + "! / (" + n + " - " + m + ")!\n= " + result;
+		Result.text = p;
+	}
+	void PrintC(int result, int n, int m){
+		string p = "";
+		p = "C(" + n + ", " + m + ")\n= " + n + "! / (" + m + "! (" + n + " - " + m + ")! )\n= " + result;
+		Result.text = p;
+	}
+	void PrintH(int result, int n, int m, int cn){
+		string p = "";
+		p = "H(" + n + ", " + m + ")\n= C(" + n + " + " + m + " - 1, " + m + ")= " + "C(" + cn + ", " + m + ")\n= " + cn + "! / (" + m + "! (" + cn + " - " + m + ")! )\n= " + result;
+		Result.text = p;
+	}
+}
