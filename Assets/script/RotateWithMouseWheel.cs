8b54222587300509981972a19d9137abffeb95c3
Greent_WU <lynn84961@gmail.com>
2017-12-21 20:09:27 +0800

    Calculator canvas edited

--- Assets/script/RotateWithMouseWheel.cs
+++ Assets/script/RotateWithMouseWheel.cs
@@ -40,6 +40,29 @@ public class RotateWithMouseWheel : MonoBehaviour {
 			transform.Rotate (new Vector3 (transform.rotation.x, transform.rotation.y, rotateSpeed * -1));
 		}
 
+		int currentAngle = (int)Mathf.Round (transform.eulerAngles.z);
+		if ((currentAngle > 342) || (currentAngle <= 18)) {
+			num = "0";
+		}else if ((currentAngle > 18) && (currentAngle <= 54)) {
+			num = "1";
+		}else if ((currentAngle > 54) && (currentAngle <= 90)) {
+			num = "2";
+		}else if ((currentAngle > 90) && (currentAngle <= 126)) {
+			num = "3";
+		}else if ((currentAngle > 126) && (currentAngle <= 162)) {
+			num = "4";
+		}else if ((currentAngle > 162) && (currentAngle <= 198)) {
+			num = "5";
+		}else if ((currentAngle > 198) && (currentAngle <= 234)) {
+			num = "6";
+		}else if ((currentAngle > 234) && (currentAngle <= 270)) {
+			num = "7";
+		}else if ((currentAngle > 270) && (currentAngle <= 306)) {
+			num = "8";
+		}else if ((currentAngle > 306) && (currentAngle <= 342)) {
+			num = "9";
+		}
+		/*
 		switch ((int)Mathf.Round(transform.eulerAngles.z))
 		{
 		case 0:
@@ -86,7 +109,7 @@ public class RotateWithMouseWheel : MonoBehaviour {
 			num = " ";
 			break;
 
-		}
+		}*/
 
 		if (currentText == 1) {
 			NumStairs.text = num;
