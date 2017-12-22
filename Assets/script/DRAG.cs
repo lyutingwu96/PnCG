f2929ad48b26752fda441520dd434989edeeecfd
laisj123 <laisj123@gmail.com>
2017-12-22 23:22:14 +0800

    added script:DRAG.cs

---
+++ Assets/script/DRAG.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DRAG : MonoBehaviour {
+	float OffsetX;
+	float OffsetY;
+
+	void Start(){
+	}
+
+	void Update(){
+	}
+
+	public void BeginDrag(){
+		OffsetX = transform.position.x - Input.mousePosition.x;
+		OffsetY = transform.position.y - Input.mousePosition.y;
+	}
+
+	public void OnDrag(){
+		transform.position = new Vector3 (Input.mousePosition.x + OffsetX, Input.mousePosition.y + OffsetY);
+	}
+}
