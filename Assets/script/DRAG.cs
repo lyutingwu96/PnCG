57370613bed0b7c1e5a455208d7a987efb4925ab
laisj123 <laisj123@gmail.com>
2017-12-23 01:49:54 +0800

    completed mouse to drag numwheel(can't show number)

--- Assets/script/DRAG.cs
+++ Assets/script/DRAG.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 
 public class DRAG : MonoBehaviour {
+	float baseAngle;
 	float OffsetX;
 	float OffsetY;
 
@@ -16,9 +17,16 @@ public class DRAG : MonoBehaviour {
 	public void BeginDrag(){
 		OffsetX = transform.position.x - Input.mousePosition.x;
 		OffsetY = transform.position.y - Input.mousePosition.y;
+		baseAngle=Mathf.Atan2(OffsetY,OffsetX)*Mathf.Rad2Deg;
+		baseAngle-=Mathf.Atan2(transform.right.y,transform.right.x)*Mathf.Rad2Deg;
+
 	}
 
 	public void OnDrag(){
-		transform.position = new Vector3 (Input.mousePosition.x + OffsetX, Input.mousePosition.y + OffsetY);
+		//transform.position = new Vector3 (Input.mousePosition.x + OffsetX, Input.mousePosition.y + OffsetY);
+		OffsetX = -(transform.position.x - Input.mousePosition.x);
+		OffsetY = -(transform.position.y - Input.mousePosition.y);
+		float ang = Mathf.Atan2 (OffsetY,OffsetX) * Mathf.Rad2Deg - baseAngle;
+		transform.rotation = Quaternion.AngleAxis (ang, Vector3.forward);
 	}
 }
